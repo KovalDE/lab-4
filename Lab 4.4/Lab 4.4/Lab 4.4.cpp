b1a309cb5f1d9457c6f6738786172c86e66a758e
KovalDE <72768859+KovalDE@users.noreply.github.com>
2020-11-02 18:37:11 +0200

    Add files via upload

---
+++ Lab 4.4/Lab 4.4/Lab 4.4.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(7) << "x" << " |"
+		<< setw(10) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x <= -4)
+			y = -2;
+		else
+			if (-4 < x && x <= 0)
+				y = -1 + ((1. / 4) * (x + 4));
+			else
+				if (x > 0 && x <= 2)
+					y = x * x;
+				else
+					y = 4 + ((-4. / 8) * (x - 2));
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
