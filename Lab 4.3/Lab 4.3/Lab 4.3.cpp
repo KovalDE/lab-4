10c0c235eea0f53b77abc2f199666f5324930361
KovalDE <72768859+KovalDE@users.noreply.github.com>
2020-11-02 18:31:10 +0200

    Add files via upload

---
+++ Lab 4.3/Lab 4.3/Lab 4.3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	double a;
+	double b;
+	double c;
+	double F;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(7) << "F   " << " |"<<endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x + c < 0 && a != 0)
+			F = ((((-a) * x * x)) - b);
+		else
+			if (x + c > 0 && a == 0)
+				F = (x - a) / (x - c);
+			else
+				F = (x / c) + (c / x);
+		cout << "|" << setw(7) << setprecision(2) << F<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
